f607947e791dd31ef20ee62f0a0ef20088b1a73a
mongkols <suphamanee81@gmail.com>
2025-02-21 01:04:36 +0700

    update3.3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const mongoose = require('mongoose');
 const path = require('path');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 'https://book-app-backend-alpha.vercel.app';
 require('dotenv').config();
 
 // Middleware
